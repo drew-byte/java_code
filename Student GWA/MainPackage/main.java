d704368570bdf853c68cdfc480c4f95946c770ea
drew <82567656+drew-byte@users.noreply.github.com>
2022-11-05 13:02:21 +0800

    Initial

---
+++ Student GWA/MainPackage/main.java
@@ -0,0 +1,86 @@
+package MainPackage;
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+import FirstStudentPackage.firstStudent;
+import SecondStudentPackage.secondStudent;
+import ThirdStudentPackage.thirdStudent;
+
+public class main {
+
+    private static String array[];
+
+    public static void main(String[] args) {
+
+        new main();
+
+    }
+
+    main() {
+        design();
+        firstStudent.getStudentName();
+        firstStudent.getGrade();
+        design();
+        secondStudent.getStudentName();
+        secondStudent.getGrade();
+        design();
+        thirdStudent.getStudentName();
+        thirdStudent.getGrade();
+        design();
+        choices();
+        design();
+    }
+
+    public static void choices() {
+        String array[] = { "", "", "", "" };
+        array[0] = firstStudent.displayName();
+        array[1] = secondStudent.displayName();
+        array[2] = thirdStudent.displayName();
+        array[3] = "EXIT";
+        System.out.println("Which student would you like to check?\n");
+        int x = 1;
+        for (int i = 0; i < 3; i++) {
+            System.out.println("     " + "[" + x + "] " + array[i]);
+            x++;
+
+        }
+        Scanner sc = new Scanner(System.in);
+        design();
+        System.out.print("Student Name: ");
+        String name = sc.nextLine();
+        if (name.equalsIgnoreCase(array[0])) {
+            System.out.println("Grade : ");
+            System.out.println("\tPhysics101: " + firstStudent.displayGrade1());
+            System.out.println("\tAlgebra101: " + firstStudent.displayGrade2());
+            System.out.println("\tProgramming: " + firstStudent.displayGrade3());
+            System.out.println(firstStudent.displayGwa());
+
+        } else if (name.equalsIgnoreCase(array[1])) {
+            System.out.println("Grade : ");
+            System.out.println("\tPhysics101: " + secondStudent.displayGrade1());
+            System.out.println("\tAlgebra101: " + secondStudent.displayGrade2());
+            System.out.println("\ttProgramming: " + secondStudent.displayGrade3());
+            System.out.println(secondStudent.displayGwa());
+
+        } else if (name.equalsIgnoreCase(array[2])) {
+            System.out.println("Grade : ");
+            System.out.println("\tPhysics101: " + thirdStudent.displayGrade1());
+            System.out.println("\tAlgebra101: " + thirdStudent.displayGrade2());
+            System.out.println("\tProgramming: " + thirdStudent.displayGrade3());
+            System.out.println(thirdStudent.displayGwa());
+
+        } else if (name.equalsIgnoreCase("EXIT")) {
+
+            System.out.println("Program Terminated");
+
+        }
+
+    }
+
+    public static void design() {
+        System.out.println("------------------------------");
+    }
+
+}
---
+++ Student GWA/ThirdStudentPackage/thirdStudent.java
@@ -0,0 +1,63 @@
+package ThirdStudentPackage;
+
+import java.util.Scanner;
+
+public class thirdStudent {
+
+    private static String firstName, lastName, display;
+    private static Integer g1, g2, g3, gwa;
+
+    public static void getStudentName() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Name: ");
+        firstName = sc.nextLine();
+        System.out.print("Enter Last Name: ");
+        lastName = sc.nextLine();
+
+    }
+
+    public static void getGrade() {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Enter Grade at Physics101: ");
+        g1 = sc1.nextInt();
+        System.out.print("Enter Grade at Algebra101: ");
+        g2 = sc1.nextInt();
+        System.out.print("Enter Grade at Programming: ");
+        g3 = sc1.nextInt();
+
+    }
+
+    public static void boundary() {
+        System.out.println("\n------------------------------\n");
+    }
+
+    public static int displayGrade1() {
+        return g1;
+
+    }
+
+    public static int displayGrade2() {
+        return g2;
+
+    }
+
+    public static int displayGrade3() {
+        return g3;
+
+    }
+
+    public static String displayGwa() {
+        gwa = (g1 + g2 + g3) / 3;
+
+        System.out.println("Your GWA is " + gwa);
+
+        display = gwa >= 75 ? "Passed!" : "Not Passed";
+
+        return display;
+    }
+
+    public static String displayName() {
+        return firstName + " " + lastName;
+    }
+}
