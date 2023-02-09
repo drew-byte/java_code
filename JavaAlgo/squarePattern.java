6ec9c2b134a25d29390d8a396946867e67df6d36
drew <shanetydrew-byte@proton.me>
2023-02-09 21:24:51 +0800

    a

---
+++ JavaAlgo/squarePattern.java
@@ -0,0 +1,15 @@
+public class squarePattern {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(j);
+                
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
