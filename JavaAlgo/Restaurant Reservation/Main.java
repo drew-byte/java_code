1b6cfbca4c27e6350c0f3ee9ee9120bf23fbc542
drew-byte <shanetydrew-byte@proton.me>
2023-05-22 22:42:30 +0800

    init

--- JavaAlgo/Restaurant Reservation/Main.java
+++ JavaAlgo/Restaurant Reservation/Main.java
@@ -2,19 +2,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main {
-
+    
+    static String choices[] = { "a.  View all Reservations", "b.  Make Reservation", "c.  Delete Reservation",
+    "d.  Generate Report", "e.  Exit" };
+    
     static ArrayList<Reservation> reserve = new ArrayList<>();
 
-    public static void main(String[] args) {
-
-        new Main();
-
-    }
-
     Main() {
-
         menu();
-
     }
 
     static void linearDisplay() {
@@ -22,47 +17,41 @@ class Main {
     }
 
     static void menu() {
-
-        System.out.println("\nSystem Menu");
-        System.out.println("\n\n\ta.  View all Reservations");
-        System.out.println("\tb.  Make Reservation");
-        System.out.println("\tc.  Delete Reservation");
-        System.out.println("\td.  Generate Report");
-        System.out.println("\te.  Exit");
-
+        linearDisplay();
+        System.out.println("\nSystem Menu\n\n");
+        for (String string : choices) {
+            System.out.println("\t" + string);
+        }
+        System.out.println();
+        linearDisplay();
         Scanner sc = new Scanner(System.in);
         System.out.print("\nInput your choice [a-e]: ");
         String ans = sc.nextLine();
-
         switch (ans) {
             case "a":
                 viewReservations();
                 break;
-
             case "b":
                 makeReservations();
                 break;
-
             case "c":
                 deleteReservation();
                 break;
-
             case "d":
                 generateReport();
                 break;
-
             case "e":
                 exit();
                 break;
-
+            default:
+                System.out.println("Please try again invalid input!");
+                menu();
+                break;
         }
-
     }
-
     static void viewReservations() {
         if (reserve.isEmpty()) {
             System.out.println("\n---------- No Data Found ----------\n");
-
         } else {
             System.out.println("");
             System.out.printf("%-5s %-10s %-10s %-10s %-10s %-10s%n", "ID", "DATE", "TIME", "NAME", "ADULTS",
@@ -74,38 +63,50 @@ class Main {
                 String newTime = R.getTime();
                 int newAdults = R.getNumOfAdults();
                 int newChildren = R.getNumOfChildren();
-
                 System.out.printf("%-5s %-10s %-10s %-10s %-10s %-10s%n", id, newDate, newTime, newName, newAdults,
                         newChildren);
                 id++;
             }
-
         }
         menu();
-
     }
-
     static void makeReservations() {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("\nName: ");
-        String inputName = sc.nextLine();
-        System.out.print("\nDate: ");
-        String inputDate = sc.nextLine();
-        System.out.print("\nTime: ");
-        String inputTime = sc.nextLine();
-        System.out.print("\nNo of Adults: ");
-        int inputAdults = sc.nextInt();
-        System.out.print("\nNo of Children: ");
-        int inputChildren = sc.nextInt();
-
-        reserve.add(new Reservation(inputName, inputDate, inputTime, inputAdults, inputChildren));
-        System.out.println("\n--------- Reservation Added ---------\n");
-        menu();
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("\nName: ");
+            String inputName = sc.nextLine();
+            System.out.print("Date: ");
+            String inputDate = sc.nextLine();
+            System.out.print("Time: ");
+            String inputTime = sc.nextLine();
+            System.out.print("No of Adults: ");
+            int inputAdults = sc.nextInt();
+            System.out.print("No of Children: ");
+            int inputChildren = sc.nextInt();
+            reserve.add(new Reservation(inputName, inputDate, inputTime, inputAdults, inputChildren));
+            System.out.println("\n--------- Reservation Added ---------\n");
+        } catch (Exception e) {
+            linearDisplay();
+            System.out.println("Invalid input. Please input an integer!");
+            System.out.println("This is another form please fill up correctly!");
+            linearDisplay();
+            makeReservations();
+        } finally {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Do you want to add more? [Y/N]: ");
+            String ans = sc.nextLine();
+            if (ans.equalsIgnoreCase("Y")) {
+                makeReservations();
+            } else if (ans.equalsIgnoreCase("N")) {
+                menu();
+            }
+        }
 
     }
 
     static void deleteReservation() {
+        deleteMenu();
+        linearDisplay();
         if (reserve.isEmpty()) {
             System.out.println("\n---------- No Data Found ----------\n");
         } else {
@@ -118,13 +119,38 @@ class Main {
             } else {
                 System.out.println("Invalid ID. No reservation found.");
             }
+
         }
         menu();
     }
 
+    static void deleteMenu() {
+        System.out.println("");
+        System.out.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s%n", "ID", "DATE", "TIME", "NAME", "ADULTS",
+                "CHILDREN", "SUBTOTAL");
+
+        int id = 1, adults = 0, child = 0, subtotal = 0;
+        for (Reservation R : reserve) {
+            String newName = R.getName();
+            String newDate = R.getDate();
+            String newTime = R.getTime();
+            int newAdults = R.getNumOfAdults();
+            int newChildren = R.getNumOfChildren();
+
+            int formulaAdults = newAdults * 500;
+            int formulaChildren = newChildren * 300;
+            int formulaSubtotal = formulaAdults + formulaChildren;
+
+            System.out.printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s%n", id, newDate, newTime, newName,
+                    newAdults, newChildren, formulaSubtotal);
+            id++;
+        }
+
+    }
+
     static void generateReport() {
         if (reserve.isEmpty()) {
-            System.out.println("\n---------- No Data Found ----------\n");
+            System.out.println("\n----------- No Data Found -----------\n");
 
         } else {
             System.out.println("");
@@ -151,10 +177,12 @@ class Main {
                 child += newChildren;
                 subtotal += formulaSubtotal;
             }
-            System.out.println("\nTotal number of Adults: " + adults);
-            System.out.println("Total number of Kids: " + child);
-            System.out.println("Grand Total: PHP " + subtotal);
-            System.out.println("\n-------------NOTHING FOLLOWS-------------\n");
+
+            String toString = "";
+
+            toString += "\nTotal number of Adults: " + adults + "\nTotal number of Kids: " + child
+                    + "\nGrand Total: PHP " + subtotal;
+            System.out.println(toString + "\n\n-------------NOTHING FOLLOWS-------------\n");
 
         }
         menu();
@@ -162,6 +190,9 @@ class Main {
     }
 
     static void exit() {
-        System.out.println("\n------------------Thank You-------------------\n");
+        System.out.println("\n-------------Thank You-------------\n");
+    }
+    public static void main(String[] args) {
+        new Main();
     }
 }
